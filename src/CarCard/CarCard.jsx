1e8079fee0fee22a2ebb5625cb7d2ea2b448ef19
agent-52 <abhaybhadauriya52@gmail.com>
2024-10-22 04:55:13 +0530

    make car card

--- src/CarCard/CarCard.jsx
+++ src/CarCard/CarCard.jsx
@@ -7,27 +7,29 @@ const CarCard = ({name="Toyota sports",airportRate, rate1, rate2, rate3, capacit
 
     return(
         <div className="flexC gap1 card">
-            <div>
+            <div className="flexC gap0">
                 <div className="carImgBox"><img src={car1} alt="" /></div>
                 {/* <div className="priceBox"><span></span></div> */}
-                <div className="reviewImgBox">
+                <div className="reviewImgBox mgI1">
                     <img src={stars} alt="image of stars for review of varanasi rental cars" />
                 </div>
             
             </div>
             
-            <h1>{name}</h1>
-            <div className="break"></div>
-            <ul>
+            <div className="flexC gap1 pi0 ">
+                <h1>{name}</h1>
+                <div className="break"></div>
+                <ul className="flexC gap0 text-sm1">
 
-                <li>Airport Pick/Drop = {airportRate}</li>
-                <li>8Hr/80Km = ₹{rate1}</li>
-                <li>12Hr/120Km = ₹{rate2}</li>
-                <li>Extra range = ₹{rate3}/Km</li>
-                <li>Driver Charge = ₹{driverRate}</li>
-                <li>Capacity = {capacity}</li>
-            </ul>
-            <button className="bookButton font-medium1 text-sm1">Book {rentName}</button>
+                    <li>Airport Pick/Drop = {airportRate}</li>
+                    <li>8Hr/80Km = ₹{rate1}</li>
+                    <li>12Hr/120Km = ₹{rate2}</li>
+                    <li>Extra range = ₹{rate3}/Km</li>
+                    <li>Driver Charge = ₹{driverRate}</li>
+                    <li>Capacity = {capacity}</li>
+                </ul>
+                <button className="bookButton font-medium1 text-sm1">Book {rentName}</button>
+            </div>
         </div>
     )
 }
