5e0b5df42f43f2b16649347015c364d038241f7e
agent-52 <abhaybhadauriya52@gmail.com>
2024-10-22 06:01:06 +0530

    add carcards with their rates and names

--- src/CarCard/CarCard.jsx
+++ src/CarCard/CarCard.jsx
@@ -3,15 +3,15 @@ import stars from "/images/logos/stars.png"
 import car1 from "/images/carImages/kia.jpg"
 
 
-const CarCard = ({name="Toyota sports",airportRate, rate1, rate2, rate3, capacity, driverRate, rentName }) =>{
+const CarCard = ({name="Toyota sports",airportRate="700", rate1, rate2, rate3, capacity, driverRate, rentName, carImage="/images/carImages/kia.jpg" }) =>{
 
     return(
         <div className="flexC gap1 card">
             <div className="flexC gap0">
-                <div className="carImgBox"><img src={car1} alt="" /></div>
+                <div className="carImgBox"><img id="carImg" src={carImage} alt="image of the respective car" /></div>
                 {/* <div className="priceBox"><span></span></div> */}
                 <div className="reviewImgBox mgI1">
-                    <img src={stars} alt="image of stars for review of varanasi rental cars" />
+                    <img  src={stars} alt="image of stars for review of varanasi rental cars" />
                 </div>
             
             </div>
@@ -19,9 +19,9 @@ const CarCard = ({name="Toyota sports",airportRate, rate1, rate2, rate3, capacit
             <div className="flexC gap1 pi0 ">
                 <h1>{name}</h1>
                 <div className="break"></div>
-                <ul className="flexC gap0 text-sm1">
+                <ul className="flexC gap0 text-sm1 text-black-a9">
 
-                    <li>Airport Pick/Drop = {airportRate}</li>
+                    <li>Airport Pick/Drop = ₹{airportRate}</li>
                     <li>8Hr/80Km = ₹{rate1}</li>
                     <li>12Hr/120Km = ₹{rate2}</li>
                     <li>Extra range = ₹{rate3}/Km</li>
