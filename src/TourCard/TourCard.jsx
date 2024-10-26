7ed124b837a4e3bcedcc093acaf4fc44125b9784
agent-52 <abhaybhadauriya52@gmail.com>
2024-10-26 13:52:57 +0530

    add icons to the travell card images size is getting smaller on mobile need to fix this

--- src/TourCard/TourCard.jsx
+++ src/TourCard/TourCard.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import "./TourCard.css"
 
-
+import rupee from "/images/logos/rupee.png"
+import time from "/images/logos/time.png"
+import location from "/images/logos/location1.png"
+import distanceIcon from "/images/logos/distance.png"
+import parking from "/images/logos/parking.png"
+import bus from "/images/logos/bus.png"
 
 
 const TourCard = ({name="",price1="8,500", distance="250",placesCovered="Triveni Sangam , Sleeping Hanuman Temple, Akshay Vat, Alopi Devi Temple,  Anand Bhavan", desc="A compact travell package from Varanasi to Prayagraj (Allahabad) designed to cover all the prominent sights between Varanasi and Prayagraj (Allahbad) like Triveni Sangam, Sleeping Hanuman Temple, Akshay Vat, Alopi Devi temple, Anand Bhavan", days="1 day", image="/images/carImages/kia.jpg" }) =>{
@@ -22,29 +27,32 @@ const TourCard = ({name="",price1="8,500", distance="250",placesCovered="Triveni
                 <ul className="flexC gap0 text-sm1 text-black-a9">
 
                     <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
+                        <div className="imgBox"><img src={rupee} alt="" /></div>
                         <div>Stating from <span>₹{price1}/-</span></div>
                     </li>
 
                     <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
+                        <div className="imgBox"><img src={time} alt="" /></div>
                         <div>Duration =  {days}</div>
                     </li>
-                    <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
-                        <div>Places =  {placesCovered}</div>
-                    </li>
+                    <div className="flexC gap00">
+                        <li className="flex gap0 alignC">
+                            <div className="imgBox"><img src={location} alt="" /></div>
+                            <h2 className="text-sm1">Places Covered :</h2>
+                        </li>
+                        <p className="pdL0"> {placesCovered}</p>
+                    </div>
                     
                     <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
-                        <div>Distance (Round trip) =  {distance}</div>
+                        <div className="imgBox"><img src={distanceIcon} alt="" /></div>
+                        <div>Distance (Round trip) =  {distance} Km</div>
                     </li>
                     <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
+                        <div className="imgBox"><img src={parking} alt="" /></div>
                         <div>Toll and Parking fare included</div>
                     </li>
                     <li className="flex gap0 alignC">
-                        <div className="imgBox"><img src="" alt="" /></div>
+                        <div className="imgBox"><img src={bus} alt="" /></div>
                         <div>Vehicles =  Urbania, Traveller(10-16 seater)</div>
                     </li>
                 </ul>
